233e85f735829b52d3ec58611c4138842b9af7f8
John Garcia <johngarciadev@gmail.com>
2022-08-11 16:23:51 -0700

    Synchronized models

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const db = require('./models/index');
+const sequelize = require('./models').sequelize;
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
@@ -55,7 +56,10 @@ app.use((err, req, res, next) => {
 // set our port
 app.set('port', process.env.PORT || 5000);
 
-// start listening on our port
-const server = app.listen(app.get('port'), () => {
-  console.log(`Express server is listening on port ${server.address().port}`);
+// Sequelize model synchronization, then start listening on our port.
+sequelize.sync()
+  .then( ()=> {
+    const server = app.listen(app.get('port'), () => {
+      console.log(`Express server is listening on port ${server.address().port}`);
+  });
 });
