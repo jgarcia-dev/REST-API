73a836e79114edb577c00c96863de0313484a9e2
John Garcia <johngarciadev@gmail.com>
2022-08-16 09:35:58 -0700

    Added validation to user model.

--- models/user.js
+++ models/user.js
@@ -13,18 +13,53 @@ module.exports = (sequelize) => {
         firstName: {
             type: Sequelize.STRING,
             allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A first name is required'
+                }
+            },
+            notEmpty: {
+                msg: 'Please provide a first name'
+            }
         },
         lastName: {
             type: Sequelize.STRING,
             allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A last name is required'
+                }
+            },
+            notEmpty: {
+                msg: 'Please provide a last name'
+            }
         },
         emailAddress: {
             type: Sequelize.STRING,
             allowNull: false,
+            unique: {
+                msg: 'That email already exists'
+            },
+            validate: {
+                notNull: {
+                    msg: 'An email is required'
+                },
+                isEmail: {
+                    msg: 'Please provide a valid email address'
+                }
+            }
         },
         password: {
             type: Sequelize.STRING,
             allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A password is required'
+                },
+                notEmpty: {
+                    msg: 'Please provide a password'
+                }
+            }
         },
     }, { sequelize });
 
